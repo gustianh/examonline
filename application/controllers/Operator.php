e7c1fd50d10ca3b6186330d041e830a389cf860c
gustianh <apps1661@gmail.com>
2019-09-20 18:01:15 +0700

    buat function

--- application/controllers/Operator.php
+++ application/controllers/Operator.php
@@ -13,10 +13,54 @@ class Admin extends CI_Controller
 
     public function index()
     {
-        /* if (!isset($this->session->admin_id))
+        $data["rows"] = $this->ambil_data();
+        $this->tampil_manage($data);
+    } 
+    public function tambah()
+    {
+        $this->tampil_edit(null);
+    }
+
+    public function edit($id)
+    {
+        $data["data"] = $this->db->get_where('administrator', array('id_administrator' => $id))->row();
+        $this->tampil_edit($data);
+    }
+
+    public function hapus($id)
+    {
+        // hapus data
+        $this->db->delete('administrator', array('id_administrator' => $id));
+
+        // tampilkan data
+        $data["message"] = "Data sudah dihapus.";
+        $data["rows"] = $this->ambil_data();
+        $this->tampil_manage($data);
+        public function simpan()
         {
-            die("error");
-        } */
+            // buat kueri
+            $data = array(
+                "username" => $this->input->post('username'),
+                "tanggal_lahir" => $this->input->post('tanggal_lahir'),
+                "jenis_kelamin" => $this->input->post('jenis_kelamin'),
+                "nidn" => $this->input->post('nidn'),
+                "jabatan" => $this->input->post('jabatan'),
+                "username" => $this->input->post('username'),
+                "password" => $this->input->post('password')
+            );
+            if ($this->input->post('id') == null) {
+                // jika tidak ada ID, maka buat data baru
+                $this->db->insert('guru', $data);
+            } else {
+                // jika ada ID, berarti edit
+                $this->db->update('guru', $data, array('id_guru' => $this->input->post("id")));
+            }
+    
+            // tampilkan data
+            $data["message"] = "Data sudah disimpan.";
+            $data["rows"] = $this->ambil_data();
+            $this->tampil_manage($data);
+        }
 
         $data["rows"] = $this->db->query('SELECT * FROM admin ORDER BY id ASC')->result();
         $this->load->view('_partial/admin_head.php');
